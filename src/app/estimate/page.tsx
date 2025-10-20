f70c8af61421d53b3894a9d04c234d3349b349ae
Max <max@gdna.io>
2025-10-20 19:21:17 -0400

    Add key prop to force table re-render when line items change
    - Added key={lastUpdateTime} to the main line items table container
    - This forces React to re-render the table when lastUpdateTime changes
    - Should fix issue where Ice & water barrier items are added but not displayed
    - The lastUpdateTime is updated whenever line items are modified in the workflow

--- src/app/estimate/page.tsx
+++ src/app/estimate/page.tsx
@@ -6234,7 +6234,7 @@ function EstimatePageContent() {
                       </button>
                     </div>
                   </div>
-                  <div className="overflow-x-auto">
+                  <div className="overflow-x-auto" key={lastUpdateTime}>
                     <table className="min-w-full divide-y divide-gray-200">
                       <thead className="bg-gray-50">
                         <tr>
